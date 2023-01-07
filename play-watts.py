282a955c3ef6b04707bc12532c4585ccedfc2007
Max Lund <maxlun@gmail.com>
2023-01-07 08:38:27 +0100

    remove test logic for flipping to next track

--- play-watts.py
+++ play-watts.py
@@ -9,16 +9,11 @@ pygame.mixer.init()
 def play_mp3s():
     for f in os.listdir('resources'):
         print(f'Playing: {f}')
-        i = 0
         if 'mp3' in f:
             sound = pygame.mixer.Sound(f'resources/{f}')
             playing = sound.play()
             while playing.get_busy():
                 pygame.time.delay(100)
-                i += 1
-                if i > 100:
-                    sound.stop()
-                    break
 
     play_mp3s()
 

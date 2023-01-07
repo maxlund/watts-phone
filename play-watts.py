baba093b77f9b8e1d0f04d9ad2cd7900d526808c
Max Lund <maxlun@gmail.com>
2023-01-07 16:31:00 +0100

    set volume based on pin input

--- play-watts.py
+++ play-watts.py
@@ -3,20 +3,29 @@
 import vlc
 import os
 import time
+import RPi.GPIO as gpio
 
 
-def play_mp3s():
+def play_mp3s(pin_number):
     for f in os.listdir('resources'):
         if f.endswith('.mp3'):
             print(f'Playing: {f}')
             player = vlc.MediaPlayer(f'resources/{f}')
             player.play()
             time.sleep(1.5)
-            duration = player.get_length() / 1000.0
-            time.sleep(duration)
+            while player.is_playing():
+                pin_input = gpio.input(pin_number)
+                if pin_input == 1:
+                    player.set_audio_volume(0)
+                else:
+                    player.set_audio_volume(100)
+                time.sleep(0.2)
 
-    play_mp3s()
+    play_mp3s(pin_number)
 
 
 if __name__ == '__main__':
-    play_mp3s()
+    pin_number = 7
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(pin_number, gpio.IN)
+    play_mp3s(pin_number)
